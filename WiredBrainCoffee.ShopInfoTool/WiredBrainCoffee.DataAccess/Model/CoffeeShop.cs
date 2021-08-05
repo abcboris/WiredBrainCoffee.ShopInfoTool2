463388bb960e8617984218b895497d2d5832225e
abcboris <sofftware.arch@gmail.com>
2021-08-05 10:35:50 -0600

    Revert "Implement Help Command"
    This reverts commit 2deaed2e56e5be7f352dd17881d07534ffe3c49e.

--- WiredBrainCoffee.ShopInfoTool/WiredBrainCoffee.DataAccess/Model/CoffeeShop.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WiredBrainCoffee.DataAccess.Model
-{
-    public class CoffeeShop
-    {
-        public string Location { get; set; }
-        public int BeansInStockInKg { get; set; }
-        //public int PaperCupsInStock { get; set; }
-    }
-}
--- WiredBrainCoffee.ShopInfoTool/WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -7,10 +7,6 @@ namespace WiredBrainCoffee.ShopInfoTool
         static void Main(string[] args)
         {
             Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
-
-
-            Console.WriteLine("Some changes here ");
-
         }
     }
 }
