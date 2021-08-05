358f8d14e80854242fca12e0db570de4151f0c74
abcboris <sofftware.arch@gmail.com>
2021-08-05 11:22:08 -0600

    Implement Quit Command

--- WiredBrainCoffee.ShopInfoTool/WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -8,7 +8,10 @@ namespace WiredBrainCoffee.ShopInfoTool
         {
             Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
 
-            Console.WriteLine("Here are my changes!");
+            Console.WriteLine("Here are my changes! changes made by Thomas blah blah");
+
+            Console.WriteLine("Some more lines added by Thomas 1");
+            Console.WriteLine("Some more lines added by Thomas 2");
         }
     }
 }
