2deaed2e56e5be7f352dd17881d07534ffe3c49e
abcboris <sofftware.arch@gmail.com>
2021-08-05 10:34:45 -0600

    Implement Help Command

--- WiredBrainCoffee.ShopInfoTool/WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -7,6 +7,10 @@ namespace WiredBrainCoffee.ShopInfoTool
         static void Main(string[] args)
         {
             Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
+
+
+            Console.WriteLine("Some changes here ");
+
         }
     }
 }
