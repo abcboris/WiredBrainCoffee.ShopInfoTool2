ecc37bbf65de8583db0bf61feae85b6de09d7946
software-arch-two <sofftware.arch2@gmail.com>
2021-08-05 11:48:16 -0600

    Add PaperCupsInStockProperty

--- WiredBrainCoffee.ShopInfoTool/WiredBrainCoffee.DataAccess/Model/CoffeeShop.cs
+++ WiredBrainCoffee.ShopInfoTool/WiredBrainCoffee.DataAccess/Model/CoffeeShop.cs
@@ -4,6 +4,6 @@
     {
         public string Location { get; set; }
         public int BeansInStockInKg { get; set; }
-        public int PaperCupsInStock { get; set; }
+        public int PaperCupsInStock { get; set; }  //Julia's change
     }
 }
